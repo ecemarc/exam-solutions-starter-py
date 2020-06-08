d999aa88d56399980d1b204f6bfead26052907fd
ecemarc <ece.marcelli@stern.nyu.edu>
2020-06-07 23:05:30 -0400

    Social!

--- solutions/social.py
+++ solutions/social.py
@@ -51,20 +51,63 @@ if __name__ == "__main__":
     #
     # "Print" the screen name of the user who authored the first tweet (i.e. "sandwhoa"):
 
+ids = []
 
-    #
-    # QUESTION B
-    #
-    # Of all the tweets which include the phrase "@sandwhoa" in their full text,
-    # ... "print" the screen name of the user who authored that tweet,
-    # ... each on a separate line (i.e. "person2", then "person3"):
+for t in tweets:
+    ids.append(t["id"])
+    min_id = min(ids)
+print([t["user"]["screen_name"] for t in tweets if t["id"] == min_id])
 
 
-    #
-    # QUESTION C
-    #
-    # Of all the tweets which include the phrase "@sandwhoa" in their full text,
-    # ... determine which tweet has the greatest number of likes,
-    # ... and then "print" the screen name of the user who authored that tweet (i.e. "person3").
-    # ... FYI: Assume the tweet order can change at any time
-    # ... and has no relationship with the number of likes.
+#
+# QUESTION B
+#
+# Of all the tweets which include the phrase "@sandwhoa" in their full text,
+# ... "print" the screen name of the user who authored that tweet,
+# ... each on a separate line (i.e. "person2", then "person3"):
+
+print("QUESTION B")
+
+tweet_body = []
+
+for t in tweets:
+    tweet_body.append(t["full_text"])
+
+new_tweet_body = list(filter(lambda k: "@sandwhoa" in k, tweet_body))
+
+for t in tweets:
+    if t["full_text"] in new_tweet_body:
+        print(t["user"]["screen_name"])
+
+
+#
+# QUESTION C
+#
+# Of all the tweets which include the phrase "@sandwhoa" in their full text,
+# ... determine which tweet has the greatest number of likes,
+# ... and then "print" the screen name of the user who authored that tweet (i.e. "person3").
+# ... FYI: Assume the tweet order can change at any time
+# ... and has no relationship with the number of likes.
+
+print("QUESTION C")
+
+# MAX FUNCTION:
+likes = []
+for t in tweets:
+    if t["full_text"] in new_tweet_body:
+        likes.append(t["likes_count"])
+        # most_likes = max(likes)
+        # print(most_likes)
+print([t["user"]["screen_name"]
+       for t in tweets if t["likes_count"] == max(likes)])
+
+# SORT FUNCTION
+
+likes = []
+for t in tweets:
+    if t["full_text"] in new_tweet_body:
+        likes.append(t["likes_count"])
+        likes.sort()
+
+print([t["user"]["screen_name"]
+       for t in tweets if t["likes_count"] == likes[-1]])
